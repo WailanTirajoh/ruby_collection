be99af72b9f3a2cfadad224aff191aafb48a6d45
Wailan Tirajoh <wailantirajoh@gmail.com>
2024-03-26 14:37:05 +0700

    fix(lint): Lintfix

--- lib/array_collection/collect.rb
+++ lib/array_collection/collect.rb
@@ -79,19 +79,23 @@ module ArrayCollection
     end
 
     def inner_join(items, left_key, right_key)
-      self.class.new(ArrayCollection::CollectionArray.inner_join(@items, get_arrayable_items(items), left_key, right_key))
+      self.class.new(ArrayCollection::CollectionArray.inner_join(@items, get_arrayable_items(items), left_key,
+                                                                 right_key))
     end
 
     def left_join(items, left_key, right_key)
-      self.class.new(ArrayCollection::CollectionArray.left_join(@items, get_arrayable_items(items), left_key, right_key))
+      self.class.new(ArrayCollection::CollectionArray.left_join(@items, get_arrayable_items(items), left_key,
+                                                                right_key))
     end
 
     def right_join(items, left_key, right_key)
-      self.class.new(ArrayCollection::CollectionArray.right_join(@items, get_arrayable_items(items), left_key, right_key))
+      self.class.new(ArrayCollection::CollectionArray.right_join(@items, get_arrayable_items(items), left_key,
+                                                                 right_key))
     end
 
     def full_join(items, left_key, right_key)
-      self.class.new(ArrayCollection::CollectionArray.full_join(@items, get_arrayable_items(items), left_key, right_key))
+      self.class.new(ArrayCollection::CollectionArray.full_join(@items, get_arrayable_items(items), left_key,
+                                                                right_key))
     end
 
     private

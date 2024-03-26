0f0c64475b45f04a6303b4179db274d2ac70a455
Wailan Tirajoh <wailantirajoh@gmail.com>
2024-03-26 11:23:45 +0700

    ci(release): add rubygem release & version bump

---
+++ .versionrc.js
@@ -0,0 +1,20 @@
+const version_rb = {
+  filename: "lib/collection/version.rb",
+  updater: {
+    readVersion: function (contents) {
+      const match = contents.match(/^\s*VERSION = ["'](.+)["']$/m);
+      return match[1];
+    },
+    writeVersion: function (contents, version) {
+      const currentVersion = this.readVersion(contents);
+      // this isn't the most reliable, but meh
+      return contents.replace(currentVersion, version);
+    },
+  },
+};
+
+module.exports = {
+  bumpFiles: [version_rb],
+  packageFiles: [version_rb],
+  preset: "angular",
+};
